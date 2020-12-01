00911da9716ca2c880d98dbe28597fef0ac0db9c
karabut <kes_n@mail.ru>
2020-12-01 21:09:57 +0700

    Initial commit

---
+++ tests.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Katya on 01.12.2020.
+//
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+using testing::Eq;
+
+class CommandTest: public testing::Test {};
+class MemoryTest: public testing::Test {};
+class TimeTests: public testing::Test {};
+
+TEST_F(CommandTest, steps) {
+    ASSERT_EQ(5, 5);
+}
+
